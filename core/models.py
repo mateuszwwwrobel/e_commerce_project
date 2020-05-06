e249fd62da2b5f9cd0c4b7d4f576a6484c9f4536
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-05-06 13:48:32 +0100

    add shipping/billing address functionality"

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.db.models.signals import post_save
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
@@ -19,6 +20,13 @@ ADDRESS_CHOICES = (
     ('B', 'Billing'),
     ('S', 'Shipping'),
 )
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, null=True, blank=True)
+    one_click_purchasing = models.BooleanField(default=False)
 
 
 class Item(models.Model):
@@ -174,3 +182,11 @@ class Refund(models.Model):
 
     def __str__(self):
         return f"{self.pk}"
+
+
+def userprofile_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        userprofile = UserProfile.objects.create(user=instance)
+
+
+post_save.connect(userprofile_receiver, sender=settings.AUTH_USER_MODEL)
